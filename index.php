194a23ba75df7c2ec9f46f76255ec10d33cb98db
Jonathan Wang <jonwzh@gmail.com>
2017-05-25 22:55:17 -0400

    Updated the grid system in index.php to be compatible with mobile devices

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="boxes col-md-4 col-md-offset-4">
+        <div class="boxes col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xs-offset-4 col-sm-offset-4 col-md-offset-4 col-lg-offset-4">
             Name: <?= $row['username']?><br />
             Time: <?= $row['date'] ?><br />
             Content:<br />
